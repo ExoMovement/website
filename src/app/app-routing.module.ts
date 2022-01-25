a8d43e6fdbb0ee7236385c0da249b7741e74f886
Sean McNamee <seanmcnamee@exo-movement.com>
2022-01-24 23:47:35 -0500

    add various pages and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { RefundReturnPolicyComponent } from './refund-return-policy/refund-return-policy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "Home", component: HomeComponent },
+  { path: "Contact", component: ContactComponent },
+  { path: "Privacy-Policy", component: PrivacyPolicyComponent },
+  { path: "Refund-Return-Policy", component: RefundReturnPolicyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
