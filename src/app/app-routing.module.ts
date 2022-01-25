9fea6c9a093562cae634378eddc37c84076ee89a
Sean McNamee <seanmcnamee@exo-movement.com>
2022-01-24 23:49:48 -0500

    add shop to nagbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { RefundReturnPolicyComponent } from './refund-return-policy/refund-return-policy.component';
+import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "Home", component: HomeComponent },
   { path: "Contact", component: ContactComponent },
   { path: "Privacy-Policy", component: PrivacyPolicyComponent },
-  { path: "Refund-Return-Policy", component: RefundReturnPolicyComponent }
+  { path: "Refund-Return-Policy", component: RefundReturnPolicyComponent },
+  { path: "Shop", component: ShopComponent },
 ];
 
 @NgModule({
