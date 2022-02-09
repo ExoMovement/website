ec2c9640b46fd2ec8e4c006ab0bab94d9e0a3278
Sean McNamee <seanmcnamee@exo-movement.com>
2022-02-08 23:13:49 -0500

    fix test files

--- src/app/views/research/our-projects/our-projects.component.spec.ts
+++ src/app/views/research/our-projects/our-projects.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { FooterComponent } from 'src/app/components/footer/footer.component';
-import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 import { OurProjectsComponent } from './our-projects.component';
 
