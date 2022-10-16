e40ace3851f07a8e72aec762fad1b603b638fea6
Sean McNamee <seanmcnamee@exo-movement.com>
2022-10-16 00:00:34 -0400

    Add font awesome and timeline section

--- src/app/views/research/research/research.component.ts
+++ src/app/views/research/research/research.component.ts
@@ -30,16 +30,31 @@ export class ResearchComponent implements OnInit {
     ]
 
     this.team = [
-      new Description("./././assets/home/team/odin.jpg", "undefined image", "Odin Francis", "Founder, Vice President, and Lead Developer", "A fourth year student at Boston University pursuing a dual degree in Mechanical Engineering with a concentration in Aerospace and Interdisciplinary Physics. Odin has experience with the engineering process as well as advanced CAD modeling, intermediate woodworking, and intermediate metalworking. He also has extensive knowledge of robotics and automotive mechanics."),
-      new Description("./././assets/home/team/no_picture.png", "undefined image", "Dylan Finn", "Head of BCI Research", "From a young age, Dylan has found a passion for computers and their potential, and recently completed his Computer Science B.S. with a concentration in Data Analytics. His interest in the Exo-Movement Project was inspired by the untimely paralyzing of a close family friend in 2006. Wanting to see this family-friend walk his daughter down the aisle is what has driven the research into Brain Computer Interfacing in the hopes of developing a brain to motor control system. Dylan is currently working as a junior developer and will be seeking to attend a graduate program to further his knowledge and make his dream a reality.	"),
-      new Description("./././assets/home/team/sean.jpg", "undefined image", "Sean McNamee", "Lead Software Developer", "Eagle Scout 2018. B.S. in Computer Science 2021. Azure Developer Associate 2022. Sean is specialized in .NET technologies, experienced with Angular, fascinated with ML and microarchitecture, and believes that every day is an opportunity for improvement. This belief holds true for anyone's knowledge, any technology, and anyone's positive impact on others. As a member of Exo-Movement, he can pursue the first, trailblaze the second, and proliferate the third."),
-      new Description("./././assets/home/team/no_picture.png", "undefined image", "Kyle Sousa", "Social Media Manager", "Finishing a Bachelor of Arts in Music, concentrating in Ethnomusicology. "),
-      new Description("./././assets/home/team/chris.png", "undefined image", "Christopher Gibson", "Research Assistant, Anatomy and Neurology", "Finishing a Bachelor of Science degree in Biology at the City College of New York (CCNY), concentration in cellular biology and molecular genetics. Teaching assistant at CCNY in General Chemistry, as well as a tutor in Introductory Biology, Genetics, and General Chemistry. Seeking to attend medical school within the next couple of years. Christopher has clinical research experience and has mastered an array of laboratory skills. "),
+      new Description("./././assets/home/team/odin.jpg", "Odin Francis Headshot", "Odin Francis", "Founder, Vice President, and Lead Developer", "A fourth year student at Boston University pursuing a dual degree in Mechanical Engineering with a concentration in Aerospace and Interdisciplinary Physics. Odin has experience with the engineering process as well as advanced CAD modeling, intermediate woodworking, and intermediate metalworking. He also has extensive knowledge of robotics and automotive mechanics."),
+      new Description("./././assets/home/team/no_picture.png", "Dylan Finn Headshot", "Dylan Finn", "Head of BCI Research", "From a young age, Dylan has found a passion for computers and their potential, and recently completed his Computer Science B.S. with a concentration in Data Analytics. His interest in the Exo-Movement Project was inspired by the untimely paralyzing of a close family friend in 2006. Wanting to see this family-friend walk his daughter down the aisle is what has driven the research into Brain Computer Interfacing in the hopes of developing a brain to motor control system. Dylan is currently working as a junior developer and will be seeking to attend a graduate program to further his knowledge and make his dream a reality.	"),
+      new Description("./././assets/home/team/sean.jpg", "Sean McNamee Headshot", "Sean McNamee", "Lead Software Developer", "Eagle Scout 2018. B.S. in Computer Science 2021. Azure Developer Associate 2022. Sean is specialized in .NET technologies, experienced with Angular, fascinated with ML and microarchitecture, and believes that every day is an opportunity for improvement. This belief holds true for anyone's knowledge, any technology, and anyone's positive impact on others. As a member of Exo-Movement, he can pursue the first, trailblaze the second, and proliferate the third."),
+      new Description("./././assets/home/team/no_picture.png", "Kyle Sousa Headshot", "Kyle Sousa", "Social Media Manager", "Finishing a Bachelor of Arts in Music, concentrating in Ethnomusicology. "),
+      new Description("./././assets/home/team/chris.png", "Christopher Gibson Headshot", "Christopher Gibson", "Research Assistant, Anatomy and Neurology", "Finishing a Bachelor of Science degree in Biology at the City College of New York (CCNY), concentration in cellular biology and molecular genetics. Teaching assistant at CCNY in General Chemistry, as well as a tutor in Introductory Biology, Genetics, and General Chemistry. Seeking to attend medical school within the next couple of years. Christopher has clinical research experience and has mastered an array of laboratory skills. "),
     ]
     this.selectedMember = this.team[0];
 
     this.timeline = [
-      new Description("", "undefined image", "Odin and Dylan found Exo-Movement", "", ""),
+      new Description("fa-lightbulb", "", "Dec 2017", "Initial concept formed", ""),
+      new Description("fa-diagram-project", "", "Jan 2018", "First CAD model created", ""),
+      new Description("fa-person", "", "Feb 2018", "Dylan Finn enters project; 1st 3D print begins for Mark 1", ""),
+      new Description("fa-skull-crossbones", "", "May 2018", "Mark 1 scraped", ""),
+      new Description("fa-arrows-to-dot", "", "Sep 2018", "Mark 2 prototype in assembly", ""),
+      new Description("fa-vial-circle-check", "", "Dec 2018", "Mark 2 prototype ready for testing", ""),
+      new Description("fa-medal", "", "Mar 2019", "1st place in the New York State Science and Engineering Fair", ""),
+      new Description("fa-medal", "", "Jun 2019", "5th place in The Neurological Surgery P.C. Health Science Competition", ""),
+      new Description("fa-skull-crossbones", "", "Jul 2019", "Mark 2 scrapped", ""),
+      new Description("fa-dollar-sign", "", "Sep 2019", "New funding, Mark 3 began", ""),
+      new Description("fa-layer-group", "", "Jan 2020", "1st CNC machined part created", ""),
+      new Description("fa-virus-covid", "", "2020", "COVID, minimal work, gained a Sean", ""),
+      new Description("fa-building", "", "May 2021", "Incorporated company", ""),
+      new Description("fa-bell-concierge", "", "Jun 2021", "Began offering services to generate revenue", ""),
+      new Description("fa-shirt", "", "Aug 2021", "Began selling branded merchandise to generate revenue", ""),
+      new Description("fa-code", "", "Oct 2021", "Began selling 3D printed tech products on eBay to generate revenue", ""),
     ];
   }
   ngOnInit(): void {
