52268f4809468439e90a1dbb6fe36f4a080f6f42
Sean McNamee <seanmcnamee@exo-movement.com>
2022-10-21 23:43:41 -0400

    fix up some colors, and add sections for the services page

--- src/app/views/services/services/services.component.ts
+++ src/app/views/services/services/services.component.ts
@@ -9,35 +9,89 @@ import { CarouselSlideItem } from '../../components/carousel/carousel.component'
 })
 export class ServicesComponent implements OnInit {
   carouselSlideItems: CarouselSlideItem[];
-  descriptions: Description[];
+  services: CardList[];
+  serviceGeneral: CardList;
+  faqs: AccordiantItem[];
 
-  constructor() { 
+  constructor() {
     this.carouselSlideItems = [
       new CarouselSlideItem("./././assets/services/CAD_Model.png", "First Slide - 3D Modeling"),
       new CarouselSlideItem("./././assets/services/Cloud-Hosting.jpg", "Second slide - Cloud Web Hosting"),
       new CarouselSlideItem("./././assets/services/OnPremise-Hosting.jpg", "Third slide - On-Premise Web Hosting"),
-    ]; 
-    this.descriptions = [
-      new Description("./././assets/home/GearLogo.png", "undefined image", "What is Exo-Movement?", "A Bio-Mechanical Company", "Exo-Movement is a for-profit company in the biomechanical field developing an exoskeleton suit for those who are paralysis victims, specifically paraplegics. The suit will provide its user more independence and improve their quality of life through a non-invasive method of regaining the ability to walk, bend over, and pick up items."),
-      new Description("./././assets/home/Spine-Vector.png", "undefined image", "Why paralysis victims?", "The undersupported populus", "There are currently 5.4 million people in America suffering from paralysis. They cannot stand, walk or lift anything without some form of assistance. Most individuals incur upwards of $2.5 million in medical bills over the course of their lives, all while being provided minimal medical options. Our device aims to ease this economic nightmare and allow these individuals to return to some form of normalcy."),
-      new Description("./././assets/home/brain.png", "undefined image", "How does it work?", "Mind-Controlled Robo-Suit", "The device is meant to assist with torso movement and lifting strength. It will be controlled through an electroencephalogram (EEG) and a series of motors and actuators. Essentially, it would control the motion of the user's upper body by reading the electrical signals from the brain, just as your muscles do now. It will maintain the natural motion paths and even offer predictive movements through a machine learning protocol (artificial intelligence)."),
-    ]
+    ];
+    this.services = [
+      new CardList("./././assets/home/GearLogo.png", "undefined image", "3D", "CAD and printing prototypes", [
+        "3D model creation",
+        "Animations",
+        "Simulations",
+        "Basic finite element analysis",
+        "Part rendering",
+      ]),
+      new CardList("./././assets/home/Spine-Vector.png", "undefined image", "Website", "Management and Maintenance", [
+        "Maintain and modify pre-existing sites",
+        "Manage hosting (3rd party company)"
+      ]), new CardList("./././assets/home/brain.png", "undefined image", "Web App", "Design and Development", [
+        "Architect infrastructure for new web applications",
+        "Develop new customized sites",
+        "Create algorithms for tackling search and other data-focused problems",
+        "Full-Stack data-driven applications"
+      ])
+    ];
+
+    this.serviceGeneral = new CardList("./././assets/home/brain.png", "undefined image", "General", "", [
+      "If there is something you want to work on and don't know where to start, email us for a free consultation and even if we can't help you directly, we can point you in the right direction",
+    ]);
+
+    this.faqs = [
+      new AccordiantItem("./././assets/home/brain.png", "undefined image", "How much is a consultation?", "", 
+        "We offer free consultations to discuss any project and give a quote for us to take on the project", 
+        true
+      ),
+      new AccordiantItem("./././assets/home/brain.png", "undefined image", "Can I get a physical prototype?", "", 
+        "We are willing to manufacture and ship any custom 3D design for a personal project/a prototype of a product",
+      ),
+      new AccordiantItem("./././assets/home/brain.png", "undefined image", "I want guidance on my ideas", "Brainstoring and planning", 
+        "We can assist in writing a project plan/brainstorming ideas even if we are not doing the project for you (for an hourly rate)",
+      ),
+    ];
   }
   ngOnInit(): void {
   }
+
+  toggleAccordianItem(accordianItem: AccordiantItem) {
+    accordianItem.isShowing = !accordianItem.isShowing;
+  }
 }
-class Description extends ImageHref {
-  static DescriptionIndex: number = 0;
+class CardList extends ImageHref {
+  static CardListIndex: number = 0;
   index: number;
   title: string;
   subtitle: string
-  description: string;
+  items: string[];
 
-  constructor(href: string, altText: string, title: string, subtitle: string, description: string) {
+  constructor(href: string, altText: string, title: string, subtitle: string, items: string[]) {
     super(href, altText);
     this.title = title;
     this.subtitle = subtitle;
-    this.description = description;
-    this.index = Description.DescriptionIndex++;
+    this.items = items;
+    this.index = CardList.CardListIndex++;
+  }
+}
+
+class AccordiantItem extends ImageHref {
+  static AccordiantItemIndex: number = 0;
+  index: number;
+  title: string;
+  subtitle: string
+  answer: string;
+  isShowing: boolean;
+
+  constructor(href: string, altText: string, title: string, subtitle: string, answer: string, showByDefault: boolean = false) {
+    super(href, altText);
+    this.title = title;
+    this.subtitle = subtitle;
+    this.answer = answer;
+    this.index = AccordiantItem.AccordiantItemIndex++;
+    this.isShowing = showByDefault;
   }
 }
